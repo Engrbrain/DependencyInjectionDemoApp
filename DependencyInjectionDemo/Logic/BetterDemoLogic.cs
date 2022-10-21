ba18f47a6a8b22820cc561b5c4b0a422ca421f8f
Uhunghama Harrison <engrbrain@gmail.com>
2022-10-22 00:24:33 +0100

    completion of different aspects and scope of dependency Injection

---
+++ DependencyInjectionDemo/Logic/BetterDemoLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DependencyInjectionDemo.Logic
+{
+	public class BetterDemoLogic: IDemoLogic
+	{
+        public int Value1 { get; private set; }
+        public int Value2 { get; private set; }
+
+        public BetterDemoLogic()
+        {
+            Value1 = Random.Shared.Next(1001, 2001);
+            Value2 = Random.Shared.Next(2001, 3001);
+        }
+    }
+}
+
---
+++ DependencyInjectionDemo/Logic/DemoLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DependencyInjectionDemo.Logic
+{
+    public class DemoLogic : IDemoLogic
+    {
+        public int Value1 { get; private set; }
+        public int Value2 { get; private set; }
+
+        public DemoLogic()
+        {
+            Value1 = Random.Shared.Next(1, 1001);
+            Value2 = Random.Shared.Next(1, 1001);
+        }
+    }
+}
+
